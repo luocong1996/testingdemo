c667b291f7d6e291114b84d27e1d5a40adf061fc
箐风 <1076607316@qq.com>
2020-04-10 10:28:21 +0800

    add cookies

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import store from './store/store'
+import VueCookies from 'vue-cookies'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(VueCookies);
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -24,6 +24,12 @@
             ...mapActions(['setUserInfo']),
             ...mapGetters(['getInfo']),
             setStore() {
+                if (!this.$cookies.isKey('user_session')) {
+                    this.$cookies.set("user_session", "test_cookies", 60 * 60 * 24);
+                } else {
+                    console.log(this.$cookies.get("user_session"));
+                    console.log(this.$cookies.keys())
+                }
                 this.setUserInfo(this.userInfo);
                 console.log(this.getInfo())
             }
